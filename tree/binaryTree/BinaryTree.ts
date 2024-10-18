cd4fb85e6253be1c02826cb3e96cd83be8a3066c
Manish Kumar Singh <manish.singh@vedantu.com>
2024-10-18 15:55:08 +0530

    Addiging BFS

--- tree/binaryTree/BinaryTree.ts
+++ tree/binaryTree/BinaryTree.ts
@@ -14,6 +14,11 @@ class BTNode {
     }
 }
 
+interface RightMostNodeData {
+    rightNode: BTNode, 
+    parent: BTNode
+}
+
 
 class BinaryTree {
     root: BTNode;
@@ -146,13 +151,41 @@ class BinaryTree {
         return result;
     }
 
-    inOrderDFS(node: BTNode) {
+    inOrderDFS(node: BTNode, result: number[]) {
         if(!node) {
-            return "";
+            return result;
         }
-        this.inOrderDFS(node.left);
-        console.log(node.value);
-        this.inOrderDFS(node.right);  
+        this.inOrderDFS(node.left, result);
+        result.push(node.value);
+        this.inOrderDFS(node.right, result);  
+        return result;
+    }
+
+    heightOfTree(node: BTNode): number {
+        if(!node) {
+            return 0;
+        }
+        return Math.max(this.heightOfTree(node.left), this.heightOfTree(node.right)) + 1;
+    }
+
+    heightOfTreeIterative(node: BTNode): number {
+        if(!node) {
+            return 0;
+        }
+        let queue = [node];        
+        let height = 0;
+        while(queue.length) {            
+            let size = queue.length;
+            // Traverse all nodes at the current level
+            for (let i = 0; i < size; i++) {
+                let temp = queue.shift();
+                if (temp.left) queue.push(temp.left);
+                if (temp.right) queue.push(temp.right);
+            }
+            // Increment height after traversing each level
+            height++;
+        }
+        return height;
     }
 
     /**
@@ -181,7 +214,84 @@ class BinaryTree {
         this.postOrderDFS(node.left);
         this.postOrderDFS(node.right);        
         console.log(node.value);
-    } 
+    }
+
+    /**
+     * Prints the values of the nodes in breadth first search order (level order)
+     * @param node {BTNode} the root of the tree to traverse
+     * @returns {number[]} the values of the nodes in the order they were visited
+     */
+    BFSTraversal(node: BTNode):number[] {
+        let result: number[] = [];
+        if(!node) {
+            return result;
+        }
+        let queue = [node];
+        let current = node;
+        while(queue.length) {
+            current = queue.shift();
+            result.push(current.value);
+            current.left && queue.push(current.left);
+            current.right && queue.push(current.right);
+        }
+        return result;
+    }
+
+    /**
+     * Gets the parent of the rightmost node in the subtree with the given root node
+     * @param node {BTNode} the root of the subtree
+     * @returns {BTNode} the parent of the rightmost node in the subtree
+     */
+    getRightMostNodeParent(node: BTNode): RightMostNodeData {
+        if(node == null) {
+           return null;
+        }
+        let result:RightMostNodeData = {rightNode: null, parent: null};        
+        let queue = [node];
+        
+        while(queue.length) {
+            result.rightNode = queue.shift();            
+            result.rightNode.left && queue.push(result.rightNode.left);
+            result.rightNode.right && queue.push(result.rightNode.right);           
+            if(result.rightNode.right || result.rightNode.left) {                
+                result.parent = result.rightNode;           
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Deletes the node with the given value from the tree. If the node to be deleted is a leaf node, it is removed. If the node to be deleted is not a leaf node, the value of the rightmost node in the subtree is swapped with the value of the node to be deleted, and the rightmost node is then removed. If the node to be deleted is not found, the tree is unchanged.
+     * @param value {number} the value of the node to be deleted
+     * @returns {BTNode} the root of the tree after deletion
+     */
+    deleteNode(value: number): BTNode {
+        if(this.root == null) {
+            return null;
+        }
+        let rightMostNodeData: RightMostNodeData = this.getRightMostNodeParent(this.root);
+        let targetNode = null;
+        let queue = [this.root];
+        let current = this.root;
+        
+        while(queue.length) {
+            current = queue.shift();
+            if(current.value == value) {
+                targetNode = current;
+                break;
+            }
+            current.left && queue.push(current.left);
+            current.right && queue.push(current.right);
+        }
+
+        if(!targetNode) {
+            return this.root;
+        }
+        
+        targetNode.value = rightMostNodeData.rightNode.value;
+        rightMostNodeData.parent.right = null;
+        return this.root;
+    }
 }
 
 let binaryTree: BinaryTree = new BinaryTree();
@@ -193,10 +303,16 @@ binaryTree.insert(5);
 binaryTree.insert(6);
 binaryTree.insert(7);
 binaryTree.insert(8);
-// console.log(JSON.stringify(binaryTree.print(binaryTree.root)));
-console.log(binaryTree.inOrderTraverseInteration(binaryTree.root));
+// binaryTree.insert(9);
+console.log(JSON.stringify(binaryTree.print(binaryTree.root)));
+// console.log(binaryTree.inOrderTraverseInteration(binaryTree.root));
 // console.log(binaryTree.preOrderTraverseInteration(binaryTree.root));
 //console.log(binaryTree.postOrderTraverseInteration(binaryTree.root));
 //binaryTree.postOrderDFS(binaryTree.root);
 // binaryTree.preOrderDFS(binaryTree.root);
-binaryTree.inOrderDFS(binaryTree.root);
+// console.log(binaryTree.inOrderDFS(binaryTree.root, []));
+//console.log(binaryTree.BFSTraversal(binaryTree.root));
+// console.log(binaryTree.deleteNode(5));
+// console.log(JSON.stringify(binaryTree.print(binaryTree.root)));
+// console.log(binaryTree.heightOfTreeIterative(binaryTree.root));
+console.log(JSON.stringify(binaryTree.getRightMostNodeParent(binaryTree.root).parent.value));
